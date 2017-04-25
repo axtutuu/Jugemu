3fb0b2d04a00796c10b6c54b4f684b3e15b8e264
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-04-25 23:35:47 +0900

    reduxの追加

--- App/App.js
+++ App/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import {Scene, Router} from 'react-native-router-flux';
 import Top   from './Components/Views/Top';
 import Settings from './Components/Views/Settings';
+import configureStore from './Stores/configureStore';
+import { Provider }    from 'react-redux';
+const store = configureStore();
 
 export default class App extends React.Component {
   render() {
-    return <Router>
-      <Scene key="root">
-        <Scene key="top" initial component={Top}/>
-        <Scene key="settings" component={Settings}/>
-      </Scene>
-    </Router>
+    return (
+      <Provider store={store}>
+        <Router>
+          <Scene key="root">
+            <Scene key="top" initial component={Top}/>
+            <Scene key="settings" component={Settings}/>
+          </Scene>
+        </Router>
+      </Provider>
+    )
   }
 }
---
+++ App/Reducers/article.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+  items: []
+};
+
+export default function article(state=INITIAL_STATE, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
